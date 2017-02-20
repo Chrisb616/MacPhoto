343cdb80262c6cd1fac510ef85711e6c277e5308
Christopher Boynton <ChrisBoynton@ETSs-MacBook-Pro.local>
2017-02-20 13:32:32 -0500

    Initial Setup

---
+++ MacPhoto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacPhoto
+//
+//  Created by Christopher Boynton on 2/20/17.
+//  Copyright © 2017 Christopher Boynton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
