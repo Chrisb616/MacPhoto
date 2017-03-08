eca17c8bc72fec45817f42faf5a1592bc11bee90
Christopher Boynton <cboynton16@gmail.com>
2017-03-07 20:56:52 -0500

    Setting up collection view

--- MacPhoto/ViewController.swift
+++ MacPhoto/ViewController.swift
@@ -10,11 +10,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var mainCollectionView: NSCollectionView!
+    @IBOutlet weak var mainCollectionViewLayout: NSCollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        configureCollectionView()
     }
 
     override var representedObject: Any? {
@@ -22,7 +24,35 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    private func configureCollectionView() {
+        mainCollectionViewLayout.itemSize = NSSize(width: 160, height: 140)
+        mainCollectionViewLayout.sectionInset = NSEdgeInsetsMake(10, 20, 10, 20)
+        mainCollectionViewLayout.minimumLineSpacing = 20
+        mainCollectionViewLayout.minimumInteritemSpacing = 20
+        mainCollectionView.collectionViewLayout = mainCollectionViewLayout
+        
+        view.wantsLayer = true
+        
+        mainCollectionView.layer = CALayer()
+        mainCollectionView.layer?.backgroundColor = NSColor.black.cgColor
+    }
 
 }
-
+extension ViewController: NSCollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return DataStore.instance.photos.count
+    }
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: "PhotoCollectionViewItem", for: indexPath)
+        guard let collectionViewItem = item as? PhotoCollectionViewItem else { return item }
+        
+        let photo = DataStore.instance.photos.at(index: indexPath.item)
+        collectionViewItem.load(photo: photo)
+        return collectionViewItem
+    }
+}
