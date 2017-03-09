7333ebcd08e33a5cdac576441acbc9d4f48b7137
Christopher Boynton <cboynton16@gmail.com>
2017-03-09 18:57:02 -0500

    Work started on person selection vc

---
+++ MacPhoto/PersonSelectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PersonSelectionViewController.swift
+//  MacPhoto
+//
+//  Created by Christopher Boynton on 3/9/17.
+//  Copyright © 2017 Christopher Boynton. All rights reserved.
+//
+
+import Cocoa
+
+class PersonSelectionViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
