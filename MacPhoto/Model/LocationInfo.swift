ba83d4eb680425db4d5313db60af468f7d1d8701
Christopher Boynton <cboynton16@gmail.com>
2019-04-22 16:13:48 -0400

    Adding location info, but leaving unused until the Encode/Decode setup is done

---
+++ MacPhoto/Model/LocationInfo.swift
@@ -0,0 +1,22 @@
+//
+//  LocationInfo.swift
+//  MacPhoto
+//
+//  Created by Christopher Boynton on 4/22/19.
+//  Copyright © 2019 Christopher Boynton. All rights reserved.
+//
+
+import Foundation
+
+struct LocationInfo {
+    var spotAddress: String?
+    var spotName: String?
+    
+    var city: String?
+    var neighborhood: String?
+    
+    var state: String?
+    var country: String?
+    
+    var continent: String?
+}
